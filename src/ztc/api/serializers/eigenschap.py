69ed342f9a88c689d8c0fb056c64ce0e88dabec6
Jos Vromans <jos@maykinmedia.nl>
2018-02-07 16:40:11 +0100

    [#16] eigenschap serializer and tests

--- src/ztc/api/serializers/eigenschap.py
+++ src/ztc/api/serializers/eigenschap.py
@@ -1,41 +1,76 @@
-from rest_framework import serializers
 
-from .zaken import ZaakTypeSerializer
-from ...datamodel.models import Eigenschap
+from rest_framework.serializers import (
+    HyperlinkedModelSerializer, ModelSerializer
+)
+from rest_framework_nested.relations import NestedHyperlinkedIdentityField
+
+from ...datamodel.models import (
+    Eigenschap, EigenschapReferentie, EigenschapSpecificatie
+)
 from ..utils.rest_flex_fields import FlexFieldsSerializerMixin
+from ..utils.serializers import SourceMappingSerializerMixin
 
 
-class EigenschapSerializer(FlexFieldsSerializerMixin, serializers.HyperlinkedModelSerializer):
-    is_van = ZaakTypeSerializer(read_only=True)
+class EigenschapReferentieSerializer(SourceMappingSerializerMixin, ModelSerializer):
+    class Meta:
+        model = EigenschapReferentie
+        source_mapping = {
+            'pathElement': 'x_path_element',
+        }
+        fields = (
+            'objecttype',
+            'informatiemodel',
+            'namespace',
+            'schemalocatie',
+            'pathElement',
+            'entiteittype',
+        )
+
+
+class EigenschapSpecificatieSerializer(SourceMappingSerializerMixin, ModelSerializer):
+    class Meta:
+        model = EigenschapSpecificatie
+        source_mapping = {
+            'waardeverzameling': 'waardenverzameling',
+        }
+        fields = (
+            'groep',
+            'formaat',
+            'lengte',
+            'kardinaliteit',
+            'waardeverzameling',
+        )
+
+
+class EigenschapSerializer(FlexFieldsSerializerMixin, SourceMappingSerializerMixin, HyperlinkedModelSerializer):
+    specificatie_van_eigenschap = EigenschapSpecificatieSerializer(read_only=True)
+    referentie_naar_eigenschap = EigenschapReferentieSerializer(read_only=True)
+
+    isVan = NestedHyperlinkedIdentityField(
+        view_name='api:zaaktype-detail',
+        parent_lookup_kwargs={
+            'catalogus_pk': 'is_van__maakt_deel_uit_van__pk',
+            'pk': 'is_van__pk'
+        },
+    )
 
     class Meta:
         model = Eigenschap
 
+        source_mapping = {
+            'ingangsdatumObject': 'datum_begin_geldigheid',
+            'einddatumObject': 'datum_einde_geldigheid',
+            'naam': 'eigenschapnaam',
+            # TODO: uncomment and add to the fields. Does not work currently..
+            # 'specificatie': 'specificatie_van_eigenschap',
+            # 'referentie': 'referentie_naar_eigenschap',
+        }
         fields = (
-            'eigenschapnaam',
+            'naam',
             'definitie',
             'toelichting',
             'status_type',
-            # 'is_van',  # TODO: currently returns all the fields from ZaakType, probably add it as a link
-
-
-            # TODO: look at xsd, use nested serializers instead of the properties
-            # 'attribuutsets' are a foreignkey, but made available as properties
-            # 'groep',
-            # 'formaat',
-            # 'lengte',
-            # 'kardinaliteit',
-            # 'waardenverzameling',
-            # 'objecttype',
-            # 'informatiemodel',
-            # 'namespace',
-            # 'schemalocatie',
-            # 'x_path_element',
-            # 'entiteittype',
+            'specificatie_van_eigenschap',
+            'referentie_naar_eigenschap',
+            'isVan',
         )
-
-    extra_kwargs = {
-        'is_van': {
-            'view_name': 'api:zaaktype-detail'
-        }
-    }
--- src/ztc/api/tests/test_eigenschap.py
+++ src/ztc/api/tests/test_eigenschap.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from ztc.datamodel.models import Eigenschap
 from ztc.datamodel.tests.base_tests import HaaglandenMixin
+from ztc.datamodel.tests.factories import (
+    EigenschapReferentieFactory, EigenschapSpecificatieFactory
+)
+
 from .base import ClientAPITestMixin
+
+EIGENSCHAP_ONE_NAAM = 'Beoogd(e) product(en'
+EIGENSCHAP_TWO_NAAM = 'Aard product'
 
 
 class EigenschapAPITests(ClientAPITestMixin, HaaglandenMixin, TestCase):
@@ -17,28 +25,70 @@ class EigenschapAPITests(ClientAPITestMixin, HaaglandenMixin, TestCase):
             'zaaktype_pk': self.zaaktype.pk
         })
 
+        #
+        # make a referentie for one Eigenschap, and a specificatie for the other one
+        #
+        self.assertEqual(Eigenschap.objects.all().count(), 2)
+        self.eigenschap_one = Eigenschap.objects.get(eigenschapnaam=EIGENSCHAP_ONE_NAAM)
+        self.eigenschap_two = Eigenschap.objects.get(eigenschapnaam=EIGENSCHAP_TWO_NAAM)
+
+        specificatie = EigenschapSpecificatieFactory.create(
+            kardinaliteit='1',
+            lengte='1',
+            groep='groep',
+        )
+        self.eigenschap_one.specificatie_van_eigenschap = specificatie
+        self.eigenschap_one.save()
+
+        referentie = EigenschapReferentieFactory.create(
+            x_path_element='x_path_element',
+            namespace='namespace',
+        )
+        self.eigenschap_two.referentie_naar_eigenschap = referentie
+        self.eigenschap_two.save()
+
     def test_get_list(self):
         response = self.api_client.get(self.eigenschap_list_url)
         self.assertEqual(response.status_code, 200)
 
         expected = {
-            '_links':
-                {'self': {
+            '_links': {
+                'self': {
                     'href': 'http://testserver/api/v1/catalogussen/{}/zaaktypen/{}/eigenschappen/'.format(self.catalogus.pk, self.zaaktype.pk)}
-                 },
-            'results': [
-                {
-                    'status_type': None,
-                    'toelichting': '',
-                    'eigenschapnaam': 'Beoogd(e) product(en',
-                    'definitie': ''
-                }, {
-                    'status_type': None,
-                    'toelichting': 'Nieuw / Verandering / Ambtshalve wijziging / Ontheffing / Intrekking',
-                    'eigenschapnaam': 'Aard product',
-                    'definitie': ''
-                }
-            ]
+            },
+            'results':
+                [
+                    {
+                        'isVan': 'http://testserver/api/v1/catalogussen/{}/zaaktypen/{}/'.format(self.catalogus.pk, self.zaaktype.pk),
+                        'status_type': None,
+                        'naam': EIGENSCHAP_ONE_NAAM,
+                        'definitie': '',
+                        'toelichting': '',
+                        'specificatie_van_eigenschap': {
+                            'formaat': '',
+                            'groep': 'groep',
+                            'kardinaliteit': '1',
+                            'lengte': '1',
+                            'waardeverzameling': []
+                        },
+                        'referentie_naar_eigenschap': None,
+                    },
+                    {
+                        'isVan': 'http://testserver/api/v1/catalogussen/{}/zaaktypen/{}/'.format(self.catalogus.pk, self.zaaktype.pk),
+                        'status_type': None,
+                        'naam': EIGENSCHAP_TWO_NAAM,
+                        'definitie': '',
+                        'specificatie_van_eigenschap': None,
+                        'toelichting': 'Nieuw / Verandering / Ambtshalve wijziging / Ontheffing / Intrekking',
+                        'referentie_naar_eigenschap': {
+                            'pathElement': 'x_path_element',
+                            'informatiemodel': None,
+                            'namespace': 'namespace',
+                            'entiteittype': '',
+                            'schemalocatie': '',
+                            'objecttype': None
+                        },
+                    }
+                ]
         }
-
         self.assertEqual(response.json(), expected)
