e757bdadf93906bae9e881fccf25ccdb202190b9
Sergei Maertens <sergei@maykinmedia.nl>
2018-05-28 14:58:51 +0200

    Doesn't work on sqlite

--- src/ztc/conf/dev.py
+++ src/ztc/conf/dev.py
@@ -12,11 +12,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'ztc.db'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ztc',
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'ztc',
+        'PASSWORD': 'ztc',
         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
     }
