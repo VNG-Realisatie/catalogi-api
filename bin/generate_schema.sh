9687c6f2e20a691917c68c56207cc8d4947488db
Sergei Maertens <sergei@maykinmedia.nl>
2018-06-04 17:43:35 +0200

    Replaced frontend tooling with OAS 3.0 toolchain

--- bin/generate_schema.sh
+++ bin/generate_schema.sh
@@ -7,7 +7,12 @@ if [[ -z "$VIRTUAL_ENV" ]]; then
     exit 1
 fi
 
+echo "Generating Swagger schema"
 src/manage.py generate_swagger \
     ./src/swagger2.0.json \
     --overwrite \
     --format=json
+
+echo "Converting Swagger to OpenAPI 3.0..."
+npm install
+npm run convert
