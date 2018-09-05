805fa34c2f150b520ea7cd26f4e223b5ba7b57ec
Sergei Maertens <sergei@maykinmedia.nl>
2018-09-05 14:52:52 +0200

    DSO API-50 tests toegevoegd

--- src/ztc/urls.py
+++ src/ztc/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
-from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path, re_path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
