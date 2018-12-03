7138c067503cbc18a5817aacb6357a2a4c05f0b9
Sergei Maertens <sergei@maykinmedia.nl>
2018-12-03 22:13:40 +0100

    :lock: Lock down permissions in ZTC

--- src/ztc/api/tests/test_auth.py
+++ src/ztc/api/tests/test_auth.py
@@ -1,6 +1,8 @@
 """
 Guarantee that the proper authorization amchinery is in place.
 """
+import uuid
+
 from rest_framework.test import APITestCase
 from zds_schema.tests import AuthCheckMixin
 
@@ -10,61 +12,62 @@ from .utils import reverse
 class ReadTests(AuthCheckMixin, APITestCase):
 
     def test_cannot_read_without_correct_scope(self):
+        dummy_uuid = str(uuid.uuid4())
         urls = [
             # root
             reverse('catalogus-list'),
-            reverse('catalogus-detail', kwargs={'uuid': 'dummy'}),
+            reverse('catalogus-detail', kwargs={'uuid': dummy_uuid}),
 
             # nested one level
             reverse('zaaktype-list', kwargs={
-                'catalogus_uuid': 'dummy'
+                'catalogus_uuid': dummy_uuid
             }),
             reverse('zaaktype-detail', kwargs={
-                'catalogus_uuid': 'dummy',
-                'uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'uuid': dummy_uuid,
             }),
             reverse('informatieobjecttype-list', kwargs={
-                'catalogus_uuid': 'dummy'
+                'catalogus_uuid': dummy_uuid
             }),
             reverse('informatieobjecttype-detail', kwargs={
-                'catalogus_uuid': 'dummy',
-                'uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'uuid': dummy_uuid,
             }),
             reverse('besluittype-list', kwargs={
-                'catalogus_uuid': 'dummy'
+                'catalogus_uuid': dummy_uuid
             }),
             reverse('besluittype-detail', kwargs={
-                'catalogus_uuid': 'dummy',
-                'uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'uuid': dummy_uuid,
             }),
 
             # nested two levels
             reverse('statustype-list', kwargs={
-                'catalogus_uuid': 'dummy',
-                'zaaktype_uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'zaaktype_uuid': dummy_uuid,
             }),
             reverse('statustype-detail', kwargs={
-                'catalogus_uuid': 'dummy',
-                'zaaktype_uuid': 'dummy',
-                'uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'zaaktype_uuid': dummy_uuid,
+                'uuid': dummy_uuid,
             }),
             reverse('eigenschap-list', kwargs={
-                'catalogus_uuid': 'dummy',
-                'zaaktype_uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'zaaktype_uuid': dummy_uuid,
             }),
             reverse('eigenschap-detail', kwargs={
-                'catalogus_uuid': 'dummy',
-                'zaaktype_uuid': 'dummy',
-                'uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'zaaktype_uuid': dummy_uuid,
+                'uuid': dummy_uuid,
             }),
             reverse('roltype-list', kwargs={
-                'catalogus_uuid': 'dummy',
-                'zaaktype_uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'zaaktype_uuid': dummy_uuid,
             }),
             reverse('roltype-detail', kwargs={
-                'catalogus_uuid': 'dummy',
-                'zaaktype_uuid': 'dummy',
-                'uuid': 'dummy',
+                'catalogus_uuid': dummy_uuid,
+                'zaaktype_uuid': dummy_uuid,
+                'uuid': dummy_uuid,
             }),
 
         ]
