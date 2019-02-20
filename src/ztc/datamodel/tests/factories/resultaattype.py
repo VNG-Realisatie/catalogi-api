84b2b58eba66a5bc4a723bdf584782f55484d063
Sergei Maertens <sergei@maykinmedia.nl>
2019-02-20 15:49:19 +0100

    :white_check_mark: Refs. #344 -- add tests for resultaattypen resource in API

--- src/ztc/datamodel/tests/factories/resultaattype.py
+++ src/ztc/datamodel/tests/factories/resultaattype.py
@@ -1,23 +1,17 @@
 import factory
 
-from ztc.datamodel.choices import ArchiefProcedure
-
 from ...models import ResultaatType
-from .relatieklassen import ZaakInformatieobjectTypeArchiefregimeFactory
 from .zaken import ZaakTypeFactory
 
 
 class ResultaatTypeFactory(factory.django.DjangoModelFactory):
-    resultaattypeomschrijving = 'omschrijving'
-    brondatum_archiefprocedure = ArchiefProcedure.eigenschap
-    archiefactietermijn = 14
-    is_relevant_voor = factory.SubFactory(ZaakTypeFactory)
+    zaaktype = factory.SubFactory(ZaakTypeFactory)
+    omschrijving = factory.Faker('word', locale='nl')
+    omschrijving_generiek = factory.Faker('url')
+    _omschrijving_generiek = factory.Faker('word')
+    selectielijstklasse = factory.Faker('url')
 
-    # call this factory with bepaalt_afwijkend_archiefregime_van = None when you dont want it
-    bepaalt_afwijkend_archiefregime_van = factory.RelatedFactory(
-        ZaakInformatieobjectTypeArchiefregimeFactory, 'resultaattype')
-
-    datum_begin_geldigheid = factory.SelfAttribute('is_relevant_voor.datum_begin_geldigheid')
+    datum_begin_geldigheid = factory.SelfAttribute('zaaktype.datum_begin_geldigheid')
 
     class Meta:
         model = ResultaatType
