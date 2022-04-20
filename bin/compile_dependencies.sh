c2f3e7c69d71959f12792121cda128226324cc45
Sergei Maertens <sergei@maykinmedia.nl>
2022-04-20 16:52:18 +0200

    :arrow_up: Bump to latest vng-api-common
    Additionally, the latest pip-tools version is now used, on Python 3.6
    still.

--- bin/compile_dependencies.sh
+++ bin/compile_dependencies.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
+#
+# Compile the dependencies for production, CI and development.
+#
+# Usage, in the root of the project:
+#
+#     ./bin/compile_dependencies.sh
+#
+# Any extra flags/arguments passed to this wrapper script are passed down to pip-compile.
+# E.g. to update a package:
+#
+#     ./bin/compile_dependencies.sh --upgrade-package django
+
 set -ex
 
 toplevel=$(git rev-parse --show-toplevel)
 
 cd $toplevel
 
+export CUSTOM_COMPILE_COMMAND="./bin/compile_dependencies.sh"
+
 # Base deps
 pip-compile \
-    --no-index \
+    --no-emit-index-url \
+    --allow-unsafe \
+    "$@" \
     requirements/base.in
 
 # Dev deps
 pip-compile \
-    --no-index \
+    --no-emit-index-url \
+    --allow-unsafe \
     --output-file requirements/dev.txt \
+    "$@" \
     requirements/base.txt \
     requirements/testing.in \
     requirements/dev.in
 
 # Jenkins/tests deps
 pip-compile \
-    --no-index \
+    --no-emit-index-url \
+    --allow-unsafe \
     --output-file requirements/jenkins.txt \
+    "$@" \
     requirements/base.txt \
     requirements/testing.in \
     requirements/jenkins.in
