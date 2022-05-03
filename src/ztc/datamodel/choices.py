04b23db770100f7ca8a0260fd9cbb2d3e76d1f8e
matthijsbekendam <matthijsbekendam@gmail.com>
2022-05-03 10:58:08 +0200

    [#1988] fixes for failed CI

--- src/ztc/datamodel/choices.py
+++ src/ztc/datamodel/choices.py
@@ -1,6 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
-from djchoices import ChoiceItem, DjangoChoices
 
+from djchoices import ChoiceItem, DjangoChoices
 
 # Waardenverzameling nemen we letterlijk over. Dit betekend dat we onder
 # andere de volgende waarden verwachten (en kleine afwijking hiervan zal dus
